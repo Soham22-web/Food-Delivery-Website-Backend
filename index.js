85af93325be62680f7cbad89099e4dc328e9341d
Soham <sohamchavan2211@gmail.com>
2025-12-11 14:40:22 +0530

    added multiple files

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
